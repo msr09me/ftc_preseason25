dac1bc3f0f4c8c28903a8e69157329ca39eabae0
msr09me <shafiqwok@gmail.com>
2025-07-31 18:57:21 -0400

    Add DriveRobot.java & Create a variable to store the speed data

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveRobot.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+public class DriveRobot {
+
+    public static void main(String[] args)
+    {
+        // Start your code below
+
+        double robotSpeed = 0.0;
+
+        System.out.println(robotSpeed);
+
+        // End your code above
+    }
+}
